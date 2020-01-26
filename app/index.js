MacroChip/MemeDB

commit eb4f2d18091d6a3710dfdda3c68f5dd3ba84d406

Remove extra spaces at the end of the joined tag string

app/index.js
@@ -22,7 +22,7 @@ db.prepare(`CREATE TABLE IF NOT EXISTS Tags (
 )`).run();
 let analyzeText = (text) => {
     return text.split(/\s+/)
-        .map(item => {
+        .filter(item => {
         if (item.length > 1 && words.check(item.toLowerCase())) {
             return item.toLowerCase();
         }

src/index.ts
@@ -11,7 +11,7 @@ db.prepare(`CREATE TABLE IF NOT EXISTS Tags (
 
 let analyzeText = (text: string) => {
   return text.split(/\s+/)
-    .map(item => {
+    .filter(item => {
       if (item.length > 1 && words.check(item.toLowerCase())) {
         return item.toLowerCase()
       }

.gitignore
@@ -1,3 +1,4 @@
 node_modules/
 *.traineddata
 *.db
+**/*.map

app/index.js.map
@@ -1,1 +1,0 @@
-{"version":3,"file":"index.js","sourceRoot":"","sources":["../src/index.ts"],"names":[],"mappings":";;;;;;;;;;;;AAAA,gEAAoC;AACpC,uCAAwB;AACxB,sDAA4B;AAC5B,MAAM,KAAK,GAAG,iBAAM,CAAC,kBAAkB,CAAC,CAAA;AACxC,oEAA0C;AAC1C,MAAM,EAAE,GAAG,wBAAa,CAAC,cAAc,CAAC,CAAA;AACxC,EAAE,CAAC,OAAO,CAAC;;;EAGT,CAAC,CAAC,GAAG,EAAE,CAAA;AAET,IAAI,WAAW,GAAG,CAAC,IAAY,EAAE,EAAE;IACjC,OAAO,IAAI,CAAC,KAAK,CAAC,KAAK,CAAC;SACrB,GAAG,CAAC,IAAI,CAAC,EAAE;QACV,IAAI,IAAI,CAAC,MAAM,GAAG,CAAC,IAAI,KAAK,CAAC,KAAK,CAAC,IAAI,CAAC,WAAW,EAAE,CAAC,EAAE;YACtD,OAAO,IAAI,CAAC,WAAW,EAAE,CAAA;SAC1B;IACH,CAAC,CAAC,CAAA;AACN,CAAC,CAAA;AAED,IAAI,SAAS,GAAG,CAAC,SAAiB,EAAE,IAAc,EAAE,EAAE;IACpD,OAAO,CAAC,GAAG,CAAC,OAAO,GAAG,IAAI,CAAC,SAAS,CAAC,IAAI,CAAC,CAAC,CAAA;IAC3C,IAAI,UAAU,GAAG,GAAG,GAAG,IAAI,CAAC,IAAI,CAAC,GAAG,CAAC,GAAG,GAAG,CAAA;IAC3C,OAAO,CAAC,GAAG,CAAC,cAAc,GAAG,IAAI,CAAC,SAAS,CAAC,UAAU,CAAC,CAAC,CAAA;IACxD,MAAM,GAAG,GAAG,EAAE,CAAC,OAAO,CAAC,6CAA6C,CAAC,CAAC,GAAG,CAAC,SAAS,EAAE,UAAU,CAAC,CAAA;IAChG,OAAO,CAAC,GAAG,CAAC,GAAG,CAAC,CAAA;AAClB,CAAC,CAAA;AAED,IAAI,KAAK,GAAG,CAAC,SAAiB,EAAE,EAAE;IAChC,EAAE,CAAC,QAAQ,CAAC,SAAS,EAAE,CAAC,GAAG,EAAE,KAAK,EAAE,EAAE;QACpC,IAAI,GAAG,EAAE;YACP,OAAO,CAAC,KAAK,CAAC,GAAG,CAAC,CAAA;YAClB,OAAM;SACP;QACD,sBAAS,CAAC,SAAS,CACjB,KAAK,EACL,KAAK,EACL,EAAE,MAAM,EAAE,CAAC,CAAC,EAAE,CAAC,OAAO,CAAC,GAAG,CAAC,CAAC,CAAC,EAAE,CAChC,CAAC,IAAI,CAAC,CAAC,EAAE,IAAI,EAAE,EAAE,IAAI,EAAE,EAAE,EAAE,EAAE;YAC5B,SAAS,CAAC,SAAS,EAAE,WAAW,CAAC,IAAI,CAAC,CAAC,CAAA;QACzC,CAAC,CAAC,CAAA;IACJ,CAAC,CAAC,CAAA;AACJ,CAAC,CAAA;AAED,IAAI,MAAM,GAAG,CAAC,GAAW,EAAE,EAAE;IAC3B,MAAM,GAAG,GAAG,EAAE,CAAC,OAAO,CAAC,yCAAyC,CAAC,CAAC,GAAG,CAAC,KAAK,GAAG,IAAI,CAAC,CAAA,CAAC,iCAAiC;IACrH,OAAO,CAAC,GAAG,CAAC,GAAG,CAAC,CAAA;AAClB,CAAC,CAAA;AAED,IAAI,OAAO,GAAW,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAA;AACrC,IAAI,OAAO,KAAK,OAAO,EAAE;IACvB,KAAK,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC,CAAA;CACvB;KAAM,IAAI,OAAO,KAAK,QAAQ,EAAE;IAC/B,MAAM,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC,CAAA;CACxB"}

commit ee96b9059d413408f79428900827032a7c5320d5

Use filter and map properly to lowercase tags which I had broken in eb4f2d1

app/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,14 +22,9 @@ db.prepare(`CREATE TABLE IF NOT EXISTS Tags (
   path TEXT,
   tags TEXT
 )`).run();
-let analyzeText = (text) => {
-    return text.split(/\s+/)
-        .filter(item => {
-        if (item.length > 1 && words.check(item.toLowerCase())) {
-            return item.toLowerCase();
-        }
-    });
-};
+let analyzeText = (text) => text.split(/\s+/)
+    .filter(item => item.length > 1 && words.check(item.toLowerCase()))
+    .map(item => item.toLowerCase());
 let storeTags = (imagePath, tags) => {
     return new Promise((res, rej) => {
         console.log("tags " + JSON.stringify(tags));
@@ -33,10 +37,10 @@ let storeTags = (imagePath, tags) => {
 };
 let index = (imagePath) => {
     fs_1.promises.stat(imagePath)
-        .then(stats => {
+        .then((stats) => __awaiter(void 0, void 0, void 0, function* () {
         if (stats.isDirectory()) {
-            return fs_1.promises.readdir(imagePath)
-                .then(files => Promise.all(files.map(file => indexSingleFile(`${imagePath}/${file}`))));
+            const files = yield fs_1.promises.readdir(imagePath);
+            return yield Promise.all(files.map(file => indexSingleFile(`${imagePath}/${file}`)));
         }
         else if (stats.isFile()) {
             return indexSingleFile(imagePath);
@@ -44,17 +48,18 @@ let index = (imagePath) => {
         else {
             console.error("Cannot search for that");
         }
-    });
+    }));
 };
-let indexSingleFile = (imagePath) => {
-    return fs_1.promises.readFile(imagePath)
-        .then(image => {
-        return tesseract_js_1.default.recognize(image, 'eng', { logger: m => console.log(m) }).then(({ data: { text } }) => {
-            return storeTags(imagePath, analyzeText(text));
-        });
-    })
-        .catch(error => console.error(error));
-};
+let indexSingleFile = (imagePath) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const image = yield fs_1.promises.readFile(imagePath);
+        const { data: { text } } = yield tesseract_js_1.default.recognize(image, 'eng', { logger: m => console.log(m) });
+        return storeTags(imagePath, analyzeText(text));
+    }
+    catch (error) {
+        return console.error(error);
+    }
+});
 let search = (tag) => {
     const row = db.prepare('SELECT path FROM Tags WHERE tags LIKE ?').all(` %${tag}% `); //TODO using LIKE is pretty flaky
     console.log(row);

src/index.ts
@@ -9,14 +9,10 @@ db.prepare(`CREATE TABLE IF NOT EXISTS Tags (
   tags TEXT
 )`).run()
 
-let analyzeText = (text: string) => { //how is a capitalized word getting through there?
-  return text.split(/\s+/)
-    .filter(item => {
-      if (item.length > 1 && words.check(item.toLowerCase())) {
-        return item.toLowerCase()
-      }
-    })
-}
+let analyzeText = (text: string) =>
+  text.split(/\s+/)
+    .filter(item => item.length > 1 && words.check(item.toLowerCase()))
+    .map(item => item.toLowerCase())
 
 let storeTags = (imagePath: string, tags: string[]) => {
   return new Promise((res, rej) => {